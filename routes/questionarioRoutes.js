1c47a99a8e467160f09823e30edf0a4897e9ca78
JoseRodriguesF <jose.antonio.220507@gmail.com>
2025-08-19 16:57:37 -0300

    feat: Criação da rota /questionario/correlacoes/3 com autenticação JWT e retornando as propriedades corretas através da procudure analisar_correlacoes_respostas(id) criada no banco

--- routes/questionarioRoutes.js
+++ routes/questionarioRoutes.js
@@ -1,7 +1,7 @@
 // Importa o pacote express para criar rotas
 import express from 'express';
 // Importa as funções do controlador de questionário
-import { getPerguntas, salvarRespostas, getPontuacaoUsuario, getHistoricoQuestionarios, getEstatisticasUsuario } from '../controllers/questionarioController.js';
+import { getPerguntas, salvarRespostas, getPontuacaoUsuario, getHistoricoQuestionarios, getEstatisticasUsuario, getCorrelacoesTendencias } from '../controllers/questionarioController.js';
 // Importa as funções do controlador de questionário diário
 import { verificarQuestionarioDiario, getPerguntasDiarias, salvarRespostasDiarias } from '../controllers/questionarioDiarioController.js';
 // Importa o middleware de autenticação para proteger as rotas
@@ -16,6 +16,7 @@ router.post('/responder', authenticate, salvarRespostas);
 router.get('/pontuacao/:usuario_id', authenticate, getPontuacaoUsuario);
 router.get('/historico/:usuario_id', authenticate, getHistoricoQuestionarios);
 router.get('/estatisticas/:usuario_id', authenticate, getEstatisticasUsuario);
+router.get('/correlacoes/:usuario_id', authenticate, getCorrelacoesTendencias);
 // Rotas do questionário diário
 router.get('/diario/verificar/:usuario_id', authenticate, verificarQuestionarioDiario);
 router.get('/diario/perguntas', authenticate, getPerguntasDiarias);
