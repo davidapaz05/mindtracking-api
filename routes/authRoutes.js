3a34a8656cfe93d3288660c297fc5fa16edd6802
JoseRodriguesF <jose.antonio.220507@gmail.com>
2025-10-20 15:37:34 -0300

    feat: rota profile para pegar informações do usuario

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 // Importa o pacote express para criar rotas
 import express from 'express';
 // Importa as funções de registro e login do controlador de autenticação
-import { register, login, verifyEmail,enviarCodigoRecuperacao, verificarCodigoRecuperacao, redefinirSenha, deleteAccount } from '../controllers/authController.js';
+import { register, login, verifyEmail,enviarCodigoRecuperacao, verificarCodigoRecuperacao, redefinirSenha, deleteAccount, getProfile } from '../controllers/authController.js';
 import authenticate from '../middlewares/authenticate.js';
 // Cria uma instância do roteador do Express
 const router = express.Router();
@@ -20,6 +20,9 @@ router.post('/verificar-codigo', verificarCodigoRecuperacao);
 router.post('/redefinir-senha', redefinirSenha);
 
 router.delete('/delete-account', authenticate, deleteAccount);
+
+// Perfil do usuário
+router.get('/profile', authenticate, getProfile);
 
 // Exporta o roteador para ser usado em outros arquivos
 export default router;
